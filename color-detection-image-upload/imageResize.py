72931d5250b0335517322e0822d37c9f4a3a1d12
mathew06 <mathewabraham6603@gmail.com>
2024-05-24 18:02:30 +0530

    change resize value

--- color-detection-image-upload/imageResize.py
+++ color-detection-image-upload/imageResize.py
@@ -1,9 +1,9 @@
 from PIL import Image
 
-# Function to resize image to 500x500 size
+# Function to resize image to specified size
 def resize_image(image):
     # Open the image
     img = Image.open(image)
-    # Resize the image to 500x500
-    img_resized = img.resize((500, 500), Image.LANCZOS)
+    # Resize the image to specified size
+    img_resized = img.resize((700, 550), Image.LANCZOS)
     return img_resized
